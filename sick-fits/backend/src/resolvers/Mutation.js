01d1ebab09be98e4ba33885d8bcddcabeeb2849d
Eric Fredine <eric.fredine@beanworks.com>
2019-02-16 15:38:36 -0800

    Completed client side graphQL section.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,13 +1,38 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        // TODO: check if they are logged in
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                ...args
-            }
-        }, info);
-        return item;
-    }
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  },
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    const item = await ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+    return item;
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = {
+      id: args.id
+    };
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // TODO Check if user owns it.
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,9 @@
+import Items from "../components/Items";
+
 const Home = props => (
-    <div>
-        <p>Hey!</p>
-    </div>
+  <div>
+    <Items page={parseFloat(props.query.page) || 1} />
+  </div>
 );
 
 export default Home;
